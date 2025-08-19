26c36c4127312e4d55b73a916a9034e8350d60c7
STUDENT <STUDENT@DESKTOP-E447JQ5>
2025-08-19 16:30:57 +0530

    dayFour

---
+++ corejavaprograms/src/Com/tnsif/dayFour/thirdPackage/Myclass.java
@@ -0,0 +1,32 @@
+//Program to demonstrate singleton class
+package Com.tnsif.dayFour.thirdPackage;
+
+public class Myclass {
+	private static Myclass obj=new Myclass();
+	private int id;
+	
+	public int getId()
+	{
+		return id;
+	}
+	
+	public void setId(int id)
+	{
+		this.id=id;
+	}
+	
+	private Myclass() {
+		System.out.println("MyClass object created");		
+	}
+	
+	public static Myclass getObject() //factory method
+	{
+		return obj;
+	}
+
+	@Override
+	public String toString() {
+		return "MyClass [id=" + id + "]";
+	}
+	
+}
