434b9a1e36cb411bbafcad6bd1e0110d29b8aea0
STUDENT <STUDENT@DESKTOP-E447JQ5>
2025-08-19 15:47:12 +0530

    singleinheritance_example

---
+++ corejavaprograms/src/single/inheritance_example/singleDemo.java
@@ -0,0 +1,11 @@
+package single.inheritance_example;
+
+public class singleDemo {
+
+	public static void main(String[] args) {
+	
+	Trainer t=new Trainer(112,"anitha","puducherry",52,"lead","Chennai/Puducherry");
+	System.out.println(t);
+	}
+
+}
