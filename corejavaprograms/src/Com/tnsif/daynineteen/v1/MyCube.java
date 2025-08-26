baf14d6a7754c80afd15a32cc36a906ac9f0b657
ANITHA <ANITHA@LAPTOP-G6GA385O>
2025-08-26 11:44:41 +0530

    daytwelve is completed

--- corejavaprograms/src/Com/tnsif/daynineteen/v1/MyCube.java
+++ corejavaprograms/src/Com/tnsif/daynineteen/v1/MyCube.java
@@ -1,5 +1,7 @@
 package Com.tnsif.daynineteen.v1;
 
-public class MyCube {
-
+@FunctionalInterface
+public interface MyCube {
+	int getCube(int no);
+	//void show(); not allow more than one abstract method
 }
---
+++ corejavaprograms/src/Com/tnsif/daytwelve/userdefinedexp/InvalidMarksException.java
@@ -0,0 +1,14 @@
+package Com.tnsif.daytwelve.userdefinedexp;
+
+public class InvalidMarksException extends Exception{
+
+	public InvalidMarksException() {
+		super();
+	}
+
+	public InvalidMarksException(String message) {
+		super(message);
+	}
+
+	
+}
